5e4c0890e32587515ca6e14ec07e27d74c6b80d8
= <=>
2025-08-06 17:33:20 +0200

    Built for speed, not for comfort

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 
 import './styles/main.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const year_element = document.getElementById('year');
+
+  year_element.textContent = new Date().getFullYear();
+});
